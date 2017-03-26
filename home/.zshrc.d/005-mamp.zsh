4ace3fb8206153a255f113608a169296691af68a
Wan Saleh <wansaleh@gmail.com>
2017-03-26 21:29:01 +0800

    Update.

--- home/.zshrc.d/005-mamp.zsh
+++ home/.zshrc.d/005-mamp.zsh
@@ -1,28 +1,16 @@
 # export PHP_VERSION="7.0.13"
 # export PATH=/Applications/MAMP/bin/php/php${PHP_VERSION}/bin:/Applications/MAMP/Library/bin:$PATH
 
-if [ -d /usr/local/mamp ]; then
-  export PATH=/usr/local/mamp:$PATH
-fi
-
-mamp_setpath() {
-  export PATH_ORIG=$PATH
-  export PATH=/usr/local/mamp:$PATH_ORIG
-}
-
-mamp_resetpath() {
-  export PATH=$PATH_ORIG
-}
+export PATH=/usr/local/mamp:$PATH
 
 mamp_link() {
   if [ ! -d /usr/local/mamp ]; then
     mkdir /usr/local/mamp
 
-    ln -s /Applications/MAMP/Library/bin/mysql* -t /usr/local/mamp/
-    ln -s /Applications/MAMP/bin/php/php7.0.13/bin/* -t /usr/local/mamp/
+    ln -sF /Applications/MAMP/Library/bin/mysql* -t /usr/local/mamp/
+    ln -sF /Applications/MAMP/bin/php/php7.0.13/bin/* -t /usr/local/mamp/
 
-    export PATH_ORIG=$PATH
-    export PATH=/usr/local/mamp:$PATH_ORIG
+    export PATH=/usr/local/mamp:$PATH
   fi
 }
 

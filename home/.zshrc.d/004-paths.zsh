fb8880a8bc00cd556641c653c81cf2f39c9f1a4f
Wan Saleh <wansaleh@gmail.com>
2017-06-28 14:00:08 +0800

    Updates.

--- home/.zshrc.d/004-paths.zsh
+++ home/.zshrc.d/004-paths.zsh
@@ -8,13 +8,6 @@ export PATH="./node_modules/.bin:$PATH"
 # Yarn binaries
 # export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
 
-# RVM
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-
 # Composer binaries
 export PATH=$HOME/.composer/vendor/bin:$PATH
 
--- home/.zshrc.d/999-last.zsh
+++ home/.zshrc.d/999-last.zsh
@@ -1,3 +1,9 @@
+# RVM
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
 # Enhancd
 # source $HOME/opt/enhancd/init.sh
 
